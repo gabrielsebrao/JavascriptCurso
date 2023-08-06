2a7fe0d25ff33e39b9ea0fae67bdd1dd390a6e83
wlfgrl417 <gabriel_sebrao@estudante.sesisenai.org.br>
2023-08-06 18:24:42 -0300

    commitado

--- Exercicios/Ex03/script.js
+++ Exercicios/Ex03/script.js
@@ -1,11 +1,44 @@
 function verificar() {
     var currentYear = new Date().getFullYear()
-    console.log(currentYear)
     var userBirthYeartxt = document.getElementById("birthYear")
-    console.log(userBirthYeartxt)
     var userBirthYear = Number(userBirthYeartxt.value)
-    console.log(userBirthYear)
-    if (userBirthYear > 2000) {
-        console.log(`oiii`)
+    var result = document.getElementById("resultado")
+    
+    if (userBirthYeartxt.value.length == 0 || userBirthYear > currentYear) {
+        window.alert('[ERRO] Insira valores validos!')
+    } else {
+        var age = currentYear - userBirthYear
+        var sex = document.getElementsByName('radsex')
+        var gender = ''
+        var img = document.createElement('img')
+        img.id = 'foto'
+        if (sex[0].checked) {
+            gender = 'Homem'
+            document.body.style.background = "#87b7ff6c"
+            if(age < 10) {
+                img.src = 'homem-crianca.png'
+            } else if (age < 21) {
+                img.src = 'homem-jovem.png'
+            } else if (age < 50) {
+                img.src = 'homem-adulto.png'
+            } else {
+                img.src = 'homem-idoso.png'
+            }
+        } else {
+            gender = 'Mulher'
+            document.body.style.background = '#ff87ed6c'
+            if(age < 10) {
+                img.src = 'mulher-crianca.png'
+            } else if (age < 21) {
+                img.src = 'mulher-jovem.png'
+            } else if (age < 50) {
+                img.src = 'mulher-adulta.png'
+            } else {
+                img.src = 'mulher-idosa.png'
+            }
+        }
+        result.style.textAlign = 'center'
+        result.innerHTML =`<p>Detectamos ${gender} com ${age} anos!`
+        result.appendChild(img)
     }
 }
